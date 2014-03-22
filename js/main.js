b983dc56dcbf023d917f4286427ce3bc0fbe732a
bmcculley <zen.fed96@gmail.com>
2014-03-22 01:40:28 -0400

    portfolio start

---
+++ js/main.js
@@ -0,0 +1,30 @@
+jQuery(document).ready(function($) {
+	var value;
+	$('.pt a').hover(function() {
+		value = $(this).data();
+		value = '.'+value['value'];
+		$(value).addClass('hover');
+	}, function() {
+		$(value).removeClass('hover');
+	});
+
+	$('.pt a').click(function(e){
+		e.preventDefault();
+		$('.modal').show();
+		$('.modal-background').show();
+		$('#title').focus();
+	});
+
+	$('.cancel-link').click(function(e){
+		e.preventDefault();
+		$('.modal').hide();
+		$('.modal-background').hide();
+	});
+
+	$(document).keyup(function(e){
+	    if(e.keyCode === 27){
+	    	$('.modal').hide();
+			$('.modal-background').hide();
+	    }
+	});
+});
