03e29202f1a09622ea2213a575cedbfaf5b8e21b
0x6f0 <enile8@yandex.com>
2018-01-03 23:38:02 -0500

    hello jekyll

--- js/site.js
+++
@@ -1,9 +0,0 @@
-$( document ).ready(function() {
-	console.log( 'document ready!' );
-	$( '#contact-form' ).addClass('hide');
-	$('.contact').on('click', function( e ) {
-		e.preventDefault();
-		$( '#contact-form' ).removeClass('hide').addClass('slideInDown animated');
-		$( '#name' ).focus();
-	});
-});
