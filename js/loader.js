357d45304539c3a4e8bf939715d22998ed12defc
bmcculley <box76135@gmail.com>
2018-06-09 03:38:59 -0400

    loader.js
    add timeout to allow jquery to load first

--- js/loader.js
+++ js/loader.js
@@ -1,9 +1,11 @@
 function loadScripts() {
     var scripts= ["https://bmcculley.github.io/js/jquery.min.js", "https://bmcculley.github.io/js/app.js"];
     for (var i = 0; i < scripts.length; i++) {
-        var script = document.createElement("script");
-        script.src = scripts[i];
-        script.type = "text/javascript";
-        document.getElementsByTagName("head")[0].appendChild(script);
+        setTimeout(function() {
+            var script = document.createElement("script");
+            script.src = scripts[i];
+            script.type = "text/javascript";
+            document.getElementsByTagName("head")[0].appendChild(script);
+        }, 500);
     }
 }
