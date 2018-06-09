ebd16b6d337f4395f1f06cd393c3a8746aaa175a
bmcculley <box76135@gmail.com>
2018-06-09 03:48:43 -0400

    loader

--- js/loader.js
+++ js/loader.js
@@ -1,13 +1,4 @@
-function loadScripts() {
-    var scripts= ["/js/jquery.min.js", "/js/app.js"];
-    for (var i = 0; i < scripts.length; i++) {
-        setTimeout(function() {
-            var script = document.createElement("script");
-            script.src = scripts[i];
-            script.type = "text/javascript";
-            document.getElementsByTagName("head")[0].appendChild(script);
-        }, 500);
-    }
+function loadStyle() {
     var linkElement = document.createElement("link");
     linkElement.rel = "stylesheet";
     linkElement.href = "/css/minimal.min.css";
