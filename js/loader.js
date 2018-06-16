fa67c49005c9be3dfb4da3b044a8d3250d67ba4c
bmcculley <box76135@gmail.com>
2018-06-15 23:34:47 -0400

    update loader
    loader should load from cloudfront and updated jquery to 3.x

--- js/loader.js
+++ js/loader.js
@@ -1,6 +1,6 @@
 function loadStyle() {
     var linkElement = document.createElement("link");
     linkElement.rel = "stylesheet";
-    linkElement.href = "/css/minimal.min.css";
+    linkElement.href = "https://d6v9x51arm3r7.cloudfront.net/css/minimal.min.css";
     document.head.appendChild(linkElement);
 }
