71265e313541495d3770c5a2ce63f4bab710f845
bmcculley <box76135@gmail.com>
2018-06-20 19:33:30 -0400

    upate css and js

--- js/loader.js
+++ js/loader.js
@@ -1,6 +1,6 @@
 function loadStyle() {
     var linkElement = document.createElement("link");
     linkElement.rel = "stylesheet";
-    linkElement.href = "https://d6v9x51arm3r7.cloudfront.net/css/minimal.min.css";
+    linkElement.href = "https://static.ejank.com/css/minimal.min.css";
     document.head.appendChild(linkElement);
 }
